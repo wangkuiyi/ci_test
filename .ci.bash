a0f2acf7dc2ccd31bc4b606ec49c777fdf1f4fe2
Yi Wang <yiwang01@baidu.com>
2016-10-07 14:45:47 -0700

    Update .ci.bash

--- .ci.bash
+++ .ci.bash
@@ -4,8 +4,5 @@ mkdir -p src/github.com/wangkuiyi/
 ln -sf `pwd`/ci_test $GOPATH/src/github.com/wangkuiyi/ci_test
 cd $GOPATH/src/github.com/wangkuiyi/ci_test
 
-echo Working in `pwd`
-echo Assuming GOPATH=$GOPATH
-
-go get -u ./...
-go run example.go
+echo GOPATH=$GOPATH go get -u ./...
+GOPATH=$GOPATH go get -u ./...
