aaeb2011febc231b96572d8e50bf6cb4a3dfa212
Yi Wang <yiwang01@baidu.com>
2016-10-07 14:32:57 -0700

    Fulfill .ci.bash for a Go project with external dependencies

--- .ci.bash
+++ .ci.bash
@@ -1,2 +1,11 @@
+cd ..
+GOPATH=`pwd`
+mkdir -p src/github.com/wangkuiyi/
+ln -sf `pwd`/ci_test $GOPATH/src/github.com/wangkuiyi/ci_test
+cd $GOPATH/src/github.com/wangkuiyi/ci_test
+
+echo Working in `pwd`
+echo Assuming GOPATH=$GOPATH
+
 go get -u ./...
 go run example.go
