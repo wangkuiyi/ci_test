fc48c90664255a0563e235916a7c87f9c52a4f6c
Yi Wang <yi.wang.2005@gmail.com>
2016-10-08 12:22:19 -0700

    Remove echo from .ci.bash

--- .ci.bash
+++ .ci.bash
@@ -4,6 +4,5 @@ mkdir -p src/github.com/wangkuiyi/
 ln -sf `pwd`/ci_test $GOPATH/src/github.com/wangkuiyi/ci_test
 cd $GOPATH/src/github.com/wangkuiyi/ci_test
 
-echo GOPATH=$GOPATH go get -u ./...
 go get -t ./... # Don't use -u
 go test ./...
